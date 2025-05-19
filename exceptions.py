09973e4725936dce7815b4749293137cd43b6fd1
KelRom <kerom97@gmail.com>
2025-05-18 23:32:32 -0500

    Did an exercise to check if a word was a palindrome. come up with two different solutions to the problem

--- exceptions.py
+++ exceptions.py
@@ -18,3 +18,32 @@ except ValueError as e:
     print(f"You didn't give me a valid number {e}")
 finally:
     print("Finally always runs")
+
+# exercise
+
+
+def is_palindrome(teststr):
+    # Your code goes here.
+    newstring = ""
+    for letter in teststr:
+        if letter.isalnum():
+            print(letter.lower())
+            newstring += letter.lower()
+
+    print(f"{newstring} is new string")
+    # reversedstring = "".join(reversed(newstring)) # original method I came up with
+    # another option that could be done. This makes more sense to me
+    reversedstring = newstring[::-1]
+    print(f"{reversedstring} is reversed string")
+    if reversedstring == newstring:
+        return True
+    return False
+
+
+# test_word = "Madam, I'm Adam."
+# test_word = "RACE CAR!"
+# test_word = "Hello, world"
+# test_word = "Radar?"
+test_word = "A man, a plan, a canal Panama!"
+
+result = is_palindrome(test_word)
